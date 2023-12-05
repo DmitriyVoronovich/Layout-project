092fe425a680a1d832fc8384868f56466a6cbf57
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-12-05 19:21:17 +0300

    added section

---
+++ src/loyout/sections/input/Input.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+import {Container} from "../../../components/Container";
+
+export const Input = () => {
+    return (
+        <StyledInput>
+            <Container>
+
+            </Container>
+        </StyledInput>
+    );
+};
+
+const StyledInput = styled.section`
+
+`
