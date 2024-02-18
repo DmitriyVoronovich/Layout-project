7e07816bd7a6fe16a4361ff064a4d6e0a78ca0f4
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-02-18 14:10:14 +0300

    code refact

--- src/styled/Theme.ts
+++ src/styled/Theme.ts
@@ -11,6 +11,6 @@ export const Theme = {
     media: {
         tablet: 'screen and (max-width: 768px)',
         mobile: 'screen and (max-width: 576px)',
-        mobile2: 'screen and (max-width: 671px)',
+        mobile2: 'screen and (max-width: 671px)'
     }
 };
