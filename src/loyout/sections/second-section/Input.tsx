5f42c05b34da76c6c1421c5c80e77ea5e646a7eb
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-04-01 11:45:27 +0300

    code refact

--- src/loyout/sections/second-section/Input.tsx
+++ src/loyout/sections/second-section/Input.tsx
@@ -20,94 +20,94 @@ export const InputSection = () => {
 };
 
 const StyledInput = styled.section`
-  max-width: 83.47vw;
-  height: 415px;
-  border: 1px solid rgba(17, 17, 21, 0);
-  background-image: url(${deliv});
-  margin: 0 auto;
+    max-width: 83.47vw;
+    height: 415px;
+    border: 1px solid rgba(17, 17, 21, 0);
+    background-image: url(${deliv});
+    margin: 0 auto;
 `
 
 const InformationBlock = styled.div`
-  position: relative;
-  width: 602px;
-  height: 243px;
-  margin: 86px 0 0 683px;
-  border-radius: 15px;
-  background-color: rgba(72, 50, 220, 0.6);
-  padding: 41px 0 0 37px;
+    position: relative;
+    width: 602px;
+    height: 243px;
+    margin: 86px 0 0 683px;
+    border-radius: 15px;
+    background-color: rgba(72, 50, 220, 0.6);
+    padding: 41px 0 0 37px;
 `
 
 const InputTitle = styled.div`
-  display: block;
-  color: #FFF;
-  text-align: start;
-  font-family: Poppins, serif;
-  font-size: 30px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
+    display: block;
+    color: #FFF;
+    text-align: start;
+    font-family: Poppins, serif;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
 `
 
 const SecondTitle = styled.span`
-  position: absolute;
-  color: #FFF;
-  text-align: center;
-  font-family: Poppins, serif;
-  font-size: 50px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  opacity: 0.18;
-  bottom: 155px;
-  left: 20px;
+    position: absolute;
+    color: #FFF;
+    text-align: center;
+    font-family: Poppins, serif;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    opacity: 0.18;
+    bottom: 155px;
+    left: 20px;
 `
 
 const Input = styled.input`
-  width: 530px;
-  height: 60px;
-  flex-shrink: 0;
-  border-radius: 10px;
-  background: #FFF;
-  padding-left: 21.5px;
-  border: 0;
-  margin-top: 16px;
-  
-  &::placeholder {
-    color: #858585;
-    text-align: start;
+    width: 530px;
+    height: 60px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    padding-left: 21.5px;
+    border: 0;
+    margin-top: 16px;
+
+    &::placeholder {
+        color: #858585;
+        text-align: start;
+        font-family: Poppins, serif;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+`
+
+const InputButton = styled.button`
+    position: absolute;
+    width: 133px;
+    height: 45px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFAE00;
+    color: #FFF;
+    text-align: center;
     font-family: Poppins, serif;
     font-size: 15px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
-  }
-`
-
-const InputButton = styled.button`
-  position: absolute;
-  width: 133px;
-  height: 45px;
-  flex-shrink: 0;
-  border-radius: 10px;
-  background: #FFAE00;
-  color: #FFF;
-  text-align: center;
-  font-family: Poppins, serif;
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  top: 110px;
-  right: 42px;
+    top: 110px;
+    right: 42px;
 `
 
 const SectionText = styled.h6`
-  color: #FFF;
-  text-align: start;
-  font-family: Poppins, serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
-  margin-top: 9.3px;
+    color: #FFF;
+    text-align: start;
+    font-family: Poppins, serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    margin-top: 9.3px;
 `
