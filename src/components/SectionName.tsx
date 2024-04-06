c1210045483c38a79e5dc40b382bbd3f6cd8b9bb
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-04-06 21:32:42 +0300

    code refact

--- src/components/SectionName.tsx
+++ src/components/SectionName.tsx
@@ -32,52 +32,54 @@ export const SectionName: React.FC<SectionNamePropsType> = (props) => {
 };
 
 const NameContainer = styled.div`
-  margin: 0 auto;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    margin: 0 auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `
 
 const SectionNameStyled = styled.span<NamePropsType>`
-  & span {
-    color: ${Theme.colors.thirdFont};
-  }
-  bottom: ${(props) => props.height};
-  position: absolute;
-  color: ${Theme.colors.primaryFont};
-  text-align: center;
-  font-family: Poppins, serif;
-  font-size: 90px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  opacity: 0.06;
-  z-index: 0;
+    & span {
+        color: ${Theme.colors.thirdFont};
+    }
+
+    bottom: ${(props) => props.height};
+    position: absolute;
+    color: ${Theme.colors.primaryFont};
+    text-align: center;
+    font-family: Poppins, serif;
+    font-size: 90px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    opacity: 0.06;
+    z-index: 0;
 `
 
 const SectionTitle = styled.h2`
-  & span {
-    color: ${Theme.colors.thirdFont};
-  }
-  margin: 59px 0  0;
-  color: ${Theme.colors.primaryFont};
-  text-align: center;
-  font-family: Poppins, serif;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  z-index: 1;
+    & span {
+        color: ${Theme.colors.thirdFont};
+    }
+
+    margin: 59px 0 0;
+    color: ${Theme.colors.primaryFont};
+    text-align: center;
+    font-family: Poppins, serif;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    z-index: 1;
 `
 
 const SectionText = styled.span`
-  color: ${Theme.colors.primaryFont};
-  text-align: center;
-  font-family: Poppins, serif;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
+    color: ${Theme.colors.primaryFont};
+    text-align: center;
+    font-family: Poppins, serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `
